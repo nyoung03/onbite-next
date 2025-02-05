1966295acd0c896cd8722a70610c54448300cf6f
nyoung03 <vnfdlvdk37@gmail.com>
2025-02-05 16:55:00 +0900

    SSG 실습

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3f900ff1d3b57698"; }
+/******/ 		__webpack_require__.h = function() { return "fb69afba109a056d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,11 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
-  // Home 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-  // 객체 반환
-  // 서버에서 실행되기 때문에 브라우저에서 읽힐 수 없음
-
+export const getStaticProps = async () => {
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks()
@@ -25,9 +21,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // 아래 콘솔 값은 서버에서 한번, 브라우저에서 한번, 총 2번 호출됨
-
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
