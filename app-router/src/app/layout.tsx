c4fb652a7f6900220b2f5967e8e73841c66b7854
nyoung03 <vnfdlvdk37@gmail.com>
2025-02-21 18:41:51 +0900

    Request Memoization

--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -1,6 +1,28 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { getSeason } from "./(with-searchbar)/page";
+import { AnimeData } from "@/types";
+
+async function Footer() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/seasons/2024/${getSeason()}?sfw`
+  );
+
+  if (!res.ok) {
+    return <footer>제작 @winterlood</footer>;
+  }
+
+  const animes: AnimeData[] = await res.json().then((res) => res.data);
+  const animesLen = animes.length;
+
+  return (
+    <footer>
+      <div>제작 @winterlood</div>
+      <div>{animesLen}개의 애니메니션이 등록되어 있습니다.</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children
@@ -15,7 +37,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE Anime</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
