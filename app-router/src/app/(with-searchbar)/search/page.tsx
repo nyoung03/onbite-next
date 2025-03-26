eb36bb9dc659f8c8db683c39130339146fd6da2c
nyoung03 <vnfdlvdk37@gmail.com>
2025-03-26 17:54:00 +0900

    Full Route cache 동적 경로에 적용

--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,19 +1,16 @@
-// 서버 컴포넌트이기 때문에 async 사용 가능
 import AnimeItem from "@/components/anime-item";
 import { AnimeData } from "@/types";
 
-// 서버 컴포넌트는 서버측에서 사전 렌더링을 위해 한번만 실행 => 비동기적으로 실행되어도 문제X
 export default async function Page({
   searchParams
 }: {
   searchParams: Promise<{ q: string }>;
 }) {
-  // query string, url param과 같이 경로상에 포함되는 값들이 컴포넌트 props로 전달됨
-
   const { q } = await searchParams;
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/anime?sfw&min_score=6&limit=10&q=${q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/anime?sfw&min_score=6&limit=10&q=${q}`,
+    { cache: "force-cache" }
   );
 
   if (!res.ok) {
--- app-router/src/app/anime/[id]/page.tsx
+++ app-router/src/app/anime/[id]/page.tsx
@@ -1,19 +1,38 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
-export default async function Page({
-  params
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const id = (await params).id;
+// generateStaticParams에 설정해둔 id 파라미터 외에 모두 다 404페이지로 보내고 싶을때
+// export const dynamicParams = false;
+
+// generateStaticParams 정적인 파라미터를 생성하는 함수
+// 아래의 3개 id 값의 페이지를 빌드타임에 정적으로 만든다.
+// 빌드 타임에 렌더링이 완료되어서 서버측에 풀라우트 캐시로써 잘 보관이 된다.
+export function generateStaticParams() {
+  // 문자열로만 명시 필요
+  // res와 같이 데이터 캐싱을 설정하지 않은 데이터 패칭이 있더라도 페이지가 스테틱 페이지로 강제 설정됨
+  return [{ id: "52991" }, { id: "16498" }, { id: "25777" }];
+}
+
+type tProps = Promise<{ id: string | string[] }>;
+
+export default async function Page({ params }: { params: tProps }) {
+  const { id } = await params;
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/anime/${id}/full`
   );
 
-  const data = await res.json().then((res) => res.data);
+  if (!res.ok) {
+    if (res.status === 404) {
+      notFound();
+    }
 
-  const { mal_id, title_english, score, synopsis, genres, images } = data;
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const data = await res.json();
+
+  const { mal_id, title_english, score, synopsis, genres, images } = data.data;
 
   return (
     <div className={style.container}>
