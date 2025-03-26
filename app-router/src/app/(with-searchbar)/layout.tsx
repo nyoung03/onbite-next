c239a49dad71dc02b383e2d35da494ee9e414bb8
nyoung03 <vnfdlvdk37@gmail.com>
2025-03-26 18:22:42 +0900

    클라이언트 라우터 캐시

--- app-router/src/app/(with-searchbar)/layout.tsx
+++ app-router/src/app/(with-searchbar)/layout.tsx
@@ -4,9 +4,9 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      {/* 클라이언트 측에서만 실행되도록 */}
-      {/* 서버측 사전 렌더링 과정에서 배제 */}
-      {/* Searchbar의 비동기 작업이 종료될 때까지 미완성 상태로 남아 있음 */}
+      {/* 해당 서치바 레이아웃은 클라이언트 라우터 캐시에 저장되어 페이지 이동을 하여도 다시 렌더링이 되고 있지 않다 */}
+      {/* 새로고침 시 재렌더링 => 클라이언트 라우터 캐시는 새로고침시 새로 렌더링 */}
+      {/* <div>{new Date().toLocaleString()}</div> */}
       <Suspense fallback={<div>Loading...</div>}>
         <Searchbar />
       </Suspense>
